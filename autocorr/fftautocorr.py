f234600b8dab68a16c1a0db2ea6a030479b78e08
Garrett Bischof <gbischof@login1.ascent.olcf.ornl.gov>
2020-08-18 11:36:48 -0400

    use mean less

--- autocorr/fftautocorr.py
+++ autocorr/fftautocorr.py
@@ -31,15 +31,28 @@ def fftautocorr(signal):
     # calculate autocorrelations for all the lag-times
     #ntimes = np.arange(N)
     g2 = np.fft.ifft(np.abs(np.fft.fft(a, axis=1))**2, axis=1).real
-    g2 = g2[:, :N] #/ np.arange(N, 0, -1)
+    #g2 = g2[:, :N] #/ np.arange(N, 0, -1)
  
     #norm = np.array([
     #    np.mean(signal[:, :N - i], axis=1) * np.mean(signal[:, i:], axis=1)
     #    for i in range(N)
     #]).T
+    temp = np.zeros_like(g2)
+    #for i in range(N):
+    #    temp[:,i] = np.mean(signal[:,i], axis=1)
+    total_sum = np.sum(signal, axis=1)
 
     norm = np.zeros_like(g2)
-    for i in range(N):
-        norm[:, i] = np.mean(signal[:, :N - i], axis=1) * np.mean(signal[:, i:], axis=1) * (N - i)
+    left = total_sum
+    right = total_sum.copy()
+    norm[:,N-1] = total_sum * total_sum / (N - 1)
+    for i in range(N-1):
+        #breakpoint()
+        left -= signal[:, N-1-i]
+        right -= signal[:, i]
+        norm[:, i] = left * right / (N - i)
+    #norm = np.zeros_like(g2)
+    #for i in range(N):
+    #    norm[:, i] = np.mean(signal[:, :N - i], axis=1) * np.mean(signal[:, i:], axis=1) * (N - i)
     
     return g2 / norm  #, ntimes
